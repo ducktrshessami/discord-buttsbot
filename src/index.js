9f391ed06023228109a4b449db65d9cbe8a692b7
ducktrshessami <jwee761@gmail.com>
2020-11-24 15:36:08 -0800

    rate is now frequency, and max is now rate

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const ios = new readline.Interface({
 
 let commands = [
     new DiscordBot.Command("restart", restart, { admin: true }),
+    new DiscordBot.Command("frequency", changeFreq, { usage: "@buttsbot frequency [number]" }),
     new DiscordBot.Command("rate", changeRate, { usage: "@buttsbot rate [number]" })
 ];
 let responses = [
@@ -56,38 +57,51 @@ function restart() {
     throw "Logging off";
 }
 
+function changeFreq(message, args) {
+    let n = parseInt(args[1]);
+    logMessage(message);
+    if (n && n > 0) {
+        config.servers[message.guild.id].freq = n;
+        updateConfig(config);
+        sendMessage(message.channel, `Buttify frequency changed to one in every \`${config.servers[message.guild.id].freq}\` messages!`);
+    }
+    else {
+        sendMessage(message.channel, `I buttify roughly one in every \`${config.servers[message.guild.id].freq}\` messages!\nTo change the frequency, use \`${this.usage}\`.\nDefault: \`${config.default.freq}\``);
+    }
+}
+
 function changeRate(message, args) {
     let n = parseInt(args[1]);
     logMessage(message);
     if (n && n > 0) {
         config.servers[message.guild.id].rate = n;
         updateConfig(config);
-        sendMessage(message.channel, `Buttify rate changed to one in every \`${config.servers[message.guild.id].rate}\` messages!`);
+        sendMessage(message.channel, `Buttify rate changed to one in every \`${config.servers[message.guild.id].rate}\` syllables per buttified message!`);
     }
     else {
-        sendMessage(message.channel, `I buttify roughly one in every \`${config.servers[message.guild.id].rate}\` messages!\nTo change this rate, use \`${this.usage}\`.\nDefault: \`${config.default.rate}\``);
+        sendMessage(message.channel, `I buttify roughly one in every \`${config.servers[message.guild.id].rate}\` syllables per buttified message!\nTo change this rate, use \`${this.usage}\`.\nDefault: \`${config.default.rate}\``);
     }
 }
 
 function checkButt(message) {
     let change = false;
+    if (!config.servers[message.guild.id].freq) {
+        change = true;
+        config.servers[message.guild.id].freq = config.default.freq;
+    }
     if (!config.servers[message.guild.id].rate) {
         change = true;
         config.servers[message.guild.id].rate = config.default.rate;
     }
-    if (!config.servers[message.guild.id].max) {
-        change = true;
-        config.servers[message.guild.id].max = config.default.max;
-    }
     if (change) {
         updateConfig(config);
     }
-    return !message.author.bot && message.guild && message.cleanContent && (Math.random() < (1 / config.servers[message.guild.id].rate));
+    return !message.author.bot && message.guild && message.cleanContent && (Math.random() < (1 / config.servers[message.guild.id].freq));
 }
 
 function sendButt(message) {
-    let buttified = buttify(message.cleanContent, config.servers[message.guild.id].max)
-    if (verifyButt(buttified)) {
+    let buttified = buttify(message.cleanContent, config.servers[message.guild.id].rate);
+    if (verifyButt(message.cleanContent, buttified)) {
         logMessage(message);
         sendMessage(message.channel, buttified);
     }
