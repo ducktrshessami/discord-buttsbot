aa01c4e4c0ab29f5f278ef3856498bdf89fe3270
ducktrshessami <jwee761@gmail.com>
2020-11-23 22:11:45 -0800

    edited stack overflow code for the purpose it's here

--- src/syllablize.js
+++ src/syllablize.js
@@ -1,5 +1,6 @@
 /*
 Separate module to declare code from Stack Overflow
+Return is edited so that it returns the syllables instead of the count
 https://stackoverflow.com/a/51175267/14184670
 */
 module.exports = function(word) 
@@ -7,12 +8,12 @@ module.exports = function(word)
     word = word.toLowerCase();                                     
     word = word.replace(/(?:[^laeiouy]|ed|[^laeiouy]e)$/, '');   
     word = word.replace(/^y/, '');                                 
-    //return word.match(/[aeiouy]{1,2}/g).length;   
+    //return word.match(/[aeiouy]{1,2}/g);   
     var syl = word.match(/[aeiouy]{1,2}/g);
     console.log(syl);
     if(syl)
     {
         //console.log(syl);
-        return syl.length;
+        return syl;
     }
 }
