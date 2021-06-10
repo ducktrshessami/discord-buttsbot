e3cb5068782549f187748d45cb67f360f59325b4
ducktrshessami <jwee761@gmail.com>
2021-06-10 16:57:46 -0700

    sync db before server listen

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ catch {
 
 const express = require("express");
 const cycle = require("express-cycle");
+const db = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -14,7 +15,14 @@ const cycler = cycle({ origin: process.env.PUBLIC_ORIGIN || `http://localhost:${
 
 app.use(cycler);
 
-app.listen(PORT, function () {
-    console.log(`Listening on PORT ${PORT}`);
-    cycler.startLoop();
-});
+db.sequelize.sync({ force: process.env.DB_FORCE && process.env.DB_FORCE.trim().toLowerCase() !== "false" })
+    .then(() => {
+        app.listen(PORT, function () {
+            console.log(`Listening on PORT ${PORT}`);
+            cycler.startLoop();
+        });
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit();
+    });
