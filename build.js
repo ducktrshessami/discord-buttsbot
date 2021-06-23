10da32c2e79dd9f9e49f16d2608c2108d08d874c
ducktrshessami <jwee761@gmail.com>
2021-06-22 23:51:14 -0700

    verbose build script

--- build.js
+++ build.js
@@ -15,11 +15,14 @@ const client = new Client();
 
 if (fs.existsSync(index)) {
     client.on("ready", () => {
+        console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`);
         fs.promises.readFile(index)
             .then(buffer => buffer.toString("utf8"))
             .then(html => html.replace("{{ client_id }}", client.user.id))
             .then(built => fs.promises.writeFile(index, built))
+            .then(() => console.log("Successfully built /public/index.html"))
             .then(() => client.destroy())
+            .then(() => console.log("Logged out"))
             .catch(console.error);
     });
 
