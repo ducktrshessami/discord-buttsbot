0e8387a25cc01b13bd55d4c0503755e753386cee
ducktrshessami <jwee761@gmail.com>
2021-08-02 13:57:24 -0700

    handle post server count request

--- bot/postServerCount.js
+++ bot/postServerCount.js
@@ -1,7 +1,18 @@
 const phin = require("phin");
 
 function postServerCount(client) {
-
+    phin({
+        url: `https://top.gg/api/bots/${client.user.id}/stats`,
+        method: "POST",
+        headers: { Authorization: process.env.TOP_TOKEN },
+        data: { server_count: client.guilds.cache.size }
+    })
+        .then(res => {
+            if (!(res.statusCode >= 200 && res.statusCode < 300)) {
+                throw new Error(`${res.statusCode} ${res.statusMessage}`);
+            }
+        })
+        .catch(console.error);
 }
 
 module.exports = postServerCount;
