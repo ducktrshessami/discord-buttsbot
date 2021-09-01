92435c06a009ddf4a245fa9d53f6d90fb586ae28
ducktrshessami <jwee761@gmail.com>
2021-09-01 15:54:38 -0700

    help command uses pages

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -4,9 +4,9 @@ const config = require("../../config/bot.json");
 
 const pages = {
     "General": [
-        "invite",
         "help",
         "ignoreme",
+        "invite",
         "prefix",
         "unignoreme"
     ],
@@ -17,13 +17,12 @@ const pages = {
         "rate",
         "unignoreall",
         "unignorechannel",
-        "unignoreme",
         "word"
     ]
 };
 
 function helpCommand(commands) {
-    var embed;
+    var embedPages;
     commands.push(new Command("help", function (message, args) {
         let cmd;
         if (args.length > 1) {
@@ -40,7 +39,7 @@ function helpCommand(commands) {
                 .catch(console.error);
         }
         else {
-            utils.replyVerbose(message, embed)
+            utils.sendPages(message.channel, embedPages, config.helpDuration)
                 .catch(console.error);
         }
     }, {
@@ -48,17 +47,15 @@ function helpCommand(commands) {
         description: "Display a command list or help text for a specific command.",
         subtitle: "<> denotes a required parameter, while [] denotes an optional one"
     }));
-    embed = {
-        embeds: [new MessageEmbed({
-            color: config.embedColor,
-            title: "Commands:",
-            fields: Object.keys(pages)
-                .map(field => ({
-                    name: field,
-                    value: pages[field].map(cmdName => `**${cmdName}:** ${commands.find(command => command.name === cmdName).description}`)
-                }))
-        })]
-    };
+    embedPages = Object.keys(pages)
+        .map(page => ({
+            embeds: [new MessageEmbed({
+                color: config.embedColor,
+                title: `${page} Commands`,
+                description: pages[page].map(cmdName => `**${cmdName}:** ${commands.find(command => command.name === cmdName).description}`)
+                    .join("\n")
+            })]
+        }));
 }
 
 module.exports = helpCommand;
