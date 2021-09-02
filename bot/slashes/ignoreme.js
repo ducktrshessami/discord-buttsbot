844a464b3ac930d704534b301c342cbe65ba7fcc
ducktrshessami <jwee761@gmail.com>
2021-09-02 15:12:43 -0700

    ignoreme slash command

---
+++ bot/slashes/ignoreme.js
@@ -0,0 +1,18 @@
+const { utils, SlashCommand } = require("discord-bot");
+const db = require("../../models");
+
+module.exports = new SlashCommand("ignoreme", function (interaction) {
+    interaction.deferReply()
+        .then(() => db.IgnoreUser.findByPk(interaction.user.id))
+        .then(ignoredUser => {
+            if (!ignoredUser) {
+                return db.IgnoreUser.create({ id: interaction.user.id })
+                    .then(() => interaction.editReply(`Okay ${this.client.config.responseEmojis.frown}`));
+            }
+            else {
+                return interaction.editReply("I'm already ignoring you.");
+            }
+        })
+        .then(utils.logMessage)
+        .catch(console.error);
+}, { description: "I will never buttify anything you say." });
