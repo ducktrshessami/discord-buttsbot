735607e284d4156ae93742218ebdf2aace4e91d7
ducktrshessami <jwee761@gmail.com>
2021-09-02 14:50:15 -0700

    invite button component

--- bot/commands/invite.js
+++ bot/commands/invite.js
@@ -1,8 +1,18 @@
 const { Command, utils } = require("discord-bot");
+const { MessageActionRow, MessageButton } = require("discord.js");
 const { permissionValue } = require("../../config/bot.json");
 
 module.exports = new Command("invite", function (message) {
-    utils.replyVerbose(message, `https://discord.com/api/oauth2/authorize?client_id=${this.client.user.id}&permissions=${permissionValue}&scope=bot`)
+    utils.replyVerbose(message, {
+        content: `Invite me to another server!`,
+        components: [new MessageActionRow({
+            components: [new MessageButton({
+                style: "LINK",
+                label: "Invite",
+                url: `https://discord.com/api/oauth2/authorize?client_id=${this.client.user.id}&permissions=${permissionValue}&scope=bot`
+            })]
+        })]
+    })
         .catch(console.error);
 }, {
     usage: "@buttsbot invite",
