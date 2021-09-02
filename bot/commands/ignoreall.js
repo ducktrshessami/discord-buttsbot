270652f914f7ceee299d4b44814b6dc36217ff84
ducktrshessami <jwee761@gmail.com>
2021-09-02 16:18:29 -0700

    fetch active threads to ignore for ignoreall

--- bot/commands/ignoreall.js
+++ bot/commands/ignoreall.js
@@ -2,16 +2,17 @@ const { Command, utils } = require("discord-bot");
 const createIgnoreChannel = require("../utils/createIgnoreChannel");
 
 module.exports = new Command("ignoreall", function (message) {
-    Promise.all(
-        message.guild.channels.cache.filter(channel =>
-            channel.type === "GUILD_TEXT" ||
-            channel.type === "GUILD_NEWS" ||
-            channel.type === "GUILD_PUBLIC_THREAD" ||
-            channel.type === "GUILD_PRIVATE_THREAD" ||
-            channel.type === "GUILD_NEWS_THREAD"
-        )
-            .map(channel => createIgnoreChannel(channel.guild.id, channel.id))
-    )
+    message.guild.channels.fetchActiveThreads()
+        .then(() => Promise.all(
+            message.guild.channels.cache.filter(channel =>
+                channel.type === "GUILD_TEXT" ||
+                channel.type === "GUILD_NEWS" ||
+                channel.type === "GUILD_PUBLIC_THREAD" ||
+                channel.type === "GUILD_PRIVATE_THREAD" ||
+                channel.type === "GUILD_NEWS_THREAD"
+            )
+                .map(channel => createIgnoreChannel(channel.guild.id, channel.id))
+        ))
         .then(() => utils.replyVerbose(message, "Okay."))
         .catch(console.error);
 }, {
