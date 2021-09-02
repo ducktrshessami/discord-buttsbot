a9f0ce64bdbd4371e8494a47fc35be8a83d418ef
ducktrshessami <jwee761@gmail.com>
2021-09-02 16:08:56 -0700

    updated channel filter for ignoreall

--- bot/commands/ignoreall.js
+++ bot/commands/ignoreall.js
@@ -3,7 +3,13 @@ const createIgnoreChannel = require("../utils/createIgnoreChannel");
 
 module.exports = new Command("ignoreall", function (message) {
     Promise.all(
-        message.guild.channels.cache.filter(channel => channel.type === "text")
+        message.guild.channels.cache.filter(channel =>
+            channel.type === "GUILD_TEXT" ||
+            channel.type === "GUILD_NEWS" ||
+            channel.type === "GUILD_PUBLIC_THREAD" ||
+            channel.type === "GUILD_PRIVATE_THREAD" ||
+            channel.type === "GUILD_NEWS_THREAD"
+        )
             .map(channel => createIgnoreChannel(channel.guild.id, channel.id))
     )
         .then(() => utils.replyVerbose(message, "Okay."))
