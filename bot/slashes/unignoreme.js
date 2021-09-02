244c9fcfb593f8ddb8ec7c270be9ce40019c7e10
ducktrshessami <jwee761@gmail.com>
2021-09-02 15:24:22 -0700

    unignoreme slash command

---
+++ bot/slashes/unignoreme.js
@@ -0,0 +1,18 @@
+const { utils, SlashCommand } = require("discord-bot");
+const db = require("../../models");
+
+module.exports = new SlashCommand("unignoreme", function (interaction) {
+    interaction.deferReply()
+        .then(() => db.IgnoreUser.findByPk(interaction.user.id))
+        .then(ignoredUser => {
+            if (ignoredUser) {
+                return ignoredUser.destroy()
+                    .then(() => interaction.editReply(`Okay ${this.client.config.responseEmojis.smile}`));
+            }
+            else {
+                return interaction.editReply("I'm not ignoring you!");
+            }
+        })
+        .then(utils.logMessage)
+        .catch(console.error);
+}, { description: "Undo ignoreme!" });
