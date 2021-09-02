f8cbb953189bfec4d6c18187a44d3ca8e2f424ea
ducktrshessami <jwee761@gmail.com>
2021-09-02 13:26:54 -0700

    store response emojis in client config

--- bot/index.js
+++ bot/index.js
@@ -7,26 +7,29 @@ const initGuild = require("./utils/initGuild");
 const botConfig = require("../config/bot.json");
 const presenceConfig = require("../config/presence.json");
 
-const smile = process.env.RES_SMILE || ":D";
-const frown = process.env.RES_FROWN || ":(";
-const wink = process.env.RES_WINK || ";)";
-const weird = process.env.RES_WEIRD || "O_o";
+const emojis = {
+    smile: process.env.RES_SMILE || ":D",
+    frown: process.env.RES_FROWN || ":(",
+    wink: process.env.RES_WINK || ";)",
+    weird: process.env.RES_WEIRD || "O_o"
+};
 
 const responses = [
-    new DiscordBot.Response(["buttsbot", "yes"], smile, responseCheck, responseSender()),
-    new DiscordBot.Response(["buttsbot", "yeah"], smile, responseCheck, responseSender()),
-    new DiscordBot.Response(["buttsbot", "yea"], smile, responseCheck, responseSender()),
-    new DiscordBot.Response(["buttsbot", "no"], frown, responseCheck, responseSender()),
-    new DiscordBot.Response(["buttsbot", "please"], wink, responseCheck, responseSender()),
-    new DiscordBot.Response(["buttsbot", "pls"], wink, responseCheck, responseSender()),
-    new DiscordBot.Response(["buttsbot", "why"], weird, responseCheck, responseSender()),
+    new DiscordBot.Response(["buttsbot", "yes"], emojis.smile, responseCheck, responseSender()),
+    new DiscordBot.Response(["buttsbot", "yeah"], emojis.smile, responseCheck, responseSender()),
+    new DiscordBot.Response(["buttsbot", "yea"], emojis.smile, responseCheck, responseSender()),
+    new DiscordBot.Response(["buttsbot", "no"], emojis.frown, responseCheck, responseSender()),
+    new DiscordBot.Response(["buttsbot", "please"], emojis.wink, responseCheck, responseSender()),
+    new DiscordBot.Response(["buttsbot", "pls"], emojis.wink, responseCheck, responseSender()),
+    new DiscordBot.Response(["buttsbot", "why"], emojis.weird, responseCheck, responseSender()),
     new DiscordBot.Response("", "", checkButt, sendButt, { requireGuild: true })
 ];
 const client = new DiscordBot({
     ...botConfig,
     token: process.env.BOT_TOKEN || botConfig.token,
     botmins: process.env.BOT_ADMINS ? JSON.parse(process.env.BOT_ADMINS) : botConfig.botmins,
-    getPrefix
+    getPrefix,
+    responseEmojis: emojis
 }, commands, undefined, responses);
 
 // Client event handling
