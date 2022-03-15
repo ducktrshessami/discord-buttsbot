9c780a2e33cbefa1c903a5ad9bde889f3de65cb3
ducktrshessami <jwee761@gmail.com>
2022-03-15 14:54:52 -0700

    async cmd freq

--- bot/commands/frequency.js
+++ bot/commands/frequency.js
@@ -2,19 +2,18 @@ const { Command, utils } = require("discord-bot");
 const db = require("../../models");
 const defaultButt = require("../../config/default.json");
 
-module.exports = new Command("frequency", function (message, args) {
+module.exports = new Command("frequency", async function (message, args) {
+    let reply;
     let n = parseInt(args[1]);
-    db.Guild.findByPk(message.guild.id)
-        .then(guild => {
-            if (n && n > 0) {
-                return guild.update({ frequency: n })
-                    .then(updated => utils.replyVerbose(message, `Buttify frequency changed to one in every \`${updated.frequency}\` messages!`));
-            }
-            else {
-                return utils.replyVerbose(message, `I buttify roughly one in every \`${guild.frequency}\` messages!\nTo change the frequency, use \`${this.usage}\`.\nDefault: \`${defaultButt.frequency}\``);
-            }
-        })
-        .catch(console.error);
+    let guild = await db.Guild.findByPk(message.guild.id);
+    if (n && n > 0) {
+        let updated = await guild.update({ frequency: n });
+        reply = `Buttify frequency changed to one in every \`${updated.frequency}\` messages!`;
+    }
+    else {
+        reply = `I buttify roughly one in every \`${guild.frequency}\` messages!\nTo change the frequency, use \`${this.usage}\`.\nDefault: \`${defaultButt.frequency}\``;
+    }
+    return utils.replyVerbose(message, reply);
 }, {
     requirePerms: "ADMINISTRATOR",
     usage: "@buttsbot frequency [number]",
