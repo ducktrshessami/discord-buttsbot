9a71174c214c459dc1493f5f06668c4293bc81a0
ducktrshessami <jwee761@gmail.com>
2022-03-15 15:14:38 -0700

    simplify cmd invite

--- bot/commands/invite.js
+++ bot/commands/invite.js
@@ -3,7 +3,7 @@ const { MessageActionRow, MessageButton } = require("discord.js");
 const { permissionValue } = require("../../config/bot.json");
 
 module.exports = new Command("invite", function (message) {
-    utils.replyVerbose(message, {
+    return utils.replyVerbose(message, {
         content: `Invite me to another server!`,
         components: [new MessageActionRow({
             components: [new MessageButton({
@@ -12,8 +12,7 @@ module.exports = new Command("invite", function (message) {
                 url: `https://discord.com/api/oauth2/authorize?client_id=${this.client.user.id}&permissions=${permissionValue}&scope=bot%20applications.commands`
             })]
         })]
-    })
-        .catch(console.error);
+    });
 }, {
     usage: "@buttsbot invite",
     description: "I'll send a link so you can invite me somewhere else!"
