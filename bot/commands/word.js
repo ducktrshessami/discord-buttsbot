be89b8a4fc0474a4b9b6e7c3de9fa3773b904db3
ducktrshessami <jwee761@gmail.com>
2022-03-17 16:38:42 -0700

    async cmd word

--- bot/commands/word.js
+++ bot/commands/word.js
@@ -2,17 +2,15 @@ const { Command, utils } = require("discord-bot");
 const db = require("../../models");
 
 module.exports = new Command("word", function (message, args) {
-    db.Guild.findByPk(message.guild.id)
-        .then(guild => {
-            if (args.length > 1) {
-                return guild.update({ word: args[1].toLowerCase() })
-                    .then(updated => utils.replyVerbose(message, `Buttification word changed to \`${updated.word}\`!`));
-            }
-            else {
-                return utils.replyVerbose(message, `I buttify messages with the word \`${guild.word}\`!`);
-            }
-        })
-        .catch(console.error);
+    let reply;
+    let guild = await db.Guild.findByPk(message.guild.id);
+    if (args.length > 1) {
+        reply = `Buttification word changed to \`${(await guild.update({ word: args[1].toLowerCase() })).word}\`!`;
+    }
+    else {
+        reply = `I buttify messages with the word \`${guild.word}\`!`;
+    }
+    return utils.replyVerbose(message, reply);
 }, {
     requirePerms: "ADMINISTRATOR",
     usage: "@buttsbot word [word]",
