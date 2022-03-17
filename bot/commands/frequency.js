2c7cd1194facb0ad14f52080cf40abfdb2588ce7
Josh Wee <jwee761@gmail.com>
2022-03-17 16:47:04 -0700

    minor styling: cmd frequency

--- bot/commands/frequency.js
+++ bot/commands/frequency.js
@@ -7,8 +7,7 @@ module.exports = new Command("frequency", async function (message, args) {
     let n = parseInt(args[1]);
     let guild = await db.Guild.findByPk(message.guild.id);
     if (n && n > 0) {
-        let updated = await guild.update({ frequency: n });
-        reply = `Buttify frequency changed to one in every \`${updated.frequency}\` messages!`;
+        reply = `Buttify frequency changed to one in every \`${(await guild.update({ frequency: n })).frequency}\` messages!`;
     }
     else {
         reply = `I buttify roughly one in every \`${guild.frequency}\` messages!\nTo change the frequency, use \`${this.usage}\`.\nDefault: \`${defaultButt.frequency}\``;
