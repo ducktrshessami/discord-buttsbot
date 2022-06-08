3ff7a1215cd4822fc417a2013a1e711deeb7b55f
ducktrshessami <jwee761@gmail.com>
2022-06-08 16:25:37 -0700

    message command collection

---
+++ discord/commands/message/index.js
@@ -0,0 +1,21 @@
+const { Collection } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+const basename = path.basename(__filename);
+
+const commands = new Collection(
+    fs
+        .readdirSync(__dirname)
+        .filter(file =>
+            (file.indexOf(".") !== 0) &&
+            (file !== basename) &&
+            (file.slice(-3) === ".js")
+        )
+        .map(file => {
+            const cmd = require(path.join(__dirname, file));
+            return [cmd.name, cmd.callback]
+        })
+);
+
+module.exports = commands;
