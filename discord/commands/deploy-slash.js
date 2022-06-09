91120e6d649ca72789c67cb0a20137765bfde82f
ducktrshessami <jwee761@gmail.com>
2022-06-09 15:17:14 -0700

    deployment script

---
+++ discord/commands/deploy-slash.js
@@ -0,0 +1,23 @@
+try {
+    require("dotenv").config();
+}
+catch {
+    console.warn("Not using dotenv. Make sure environment variables are set");
+}
+
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const slashCommands = require("./slash");
+
+const env = process.env.NODE_ENV || "development";
+const commands = slashCommands.map(command => command.data.toJSON());
+const rest = new REST({ version: "9" })
+    .setToken(process.env.DISCORD_TOKEN);
+
+rest.put(
+    env === "production" ?
+        Routes.applicationCommands(process.env.DISCORD_CLIENTID) :
+        Routes.applicationGuildCommands(process.env.DISCORD_CLIENTID, process.env.DISCORD_TESTGUILD),
+    { body: commands }
+)
+    .catch(console.error);
