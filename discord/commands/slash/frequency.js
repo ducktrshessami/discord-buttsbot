e4b95a4bf6d50e72a4dfdd9ebe54ce5eacb2eaca
Josh Fish <jwee761@gmail.com>
2022-06-12 15:09:06 -0700

    frequency slash callback

--- discord/commands/slash/frequency.js
+++ discord/commands/slash/frequency.js
@@ -1,5 +1,8 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Permissions } = require("discord.js");
+const db = require("../../../models");
+const logMessage = require("../../utils/logMessage");
+const defaultButt = require("../../../config/default.json");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,10 +13,21 @@ module.exports = {
         .addIntegerOption(option =>
             option
                 .setName("value")
-                .setDescription("A new frequency to buttify messages!")
+                .setDescription(`A new frequency to buttify messages! The lower this is, the more I'll buttify! The default is ${defaultButt.frequency}.`)
                 .setMinValue(1)
         ),
     callback: async function (interaction) {
-
+        let reply;
+        const newValue = interaction.options.getInteger("value");
+        await interaction.deferReply();
+        const guildModel = await db.Guild.findByPk(interaction.guildId);
+        if (newValue) {
+            const { frequency } = await guildModel.update({ frequency: newValue });
+            reply = `Buttify frequency changed to one in every \`${frequency}\` messages!`;
+        }
+        else {
+            reply = `I buttify roughly one in every \`${guildModel.frequency}\` messages!`;
+        }
+        logMessage(await interaction.editReply(reply));
     }
 };
