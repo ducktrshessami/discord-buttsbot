2aae8983551331af2f0577867c764d5c9509d84b
Josh Fish <jwee761@gmail.com>
2022-06-12 15:42:44 -0700

    ignoreall slash callback

--- discord/commands/slash/ignoreall.js
+++ discord/commands/slash/ignoreall.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Permissions } = require("discord.js");
+const db = require("../../../models");
+const logMessage = require("../../utils/logMessage");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,7 +9,19 @@ module.exports = {
         .setDescription("I won't buttify in any channel.")
         .setDMPermission(false)
         .setDefaultMemberPermissions(Permissions.FLAGS.MANAGE_GUILD),
-    callback: function (interaction) {
-
+    callback: async function (interaction) {
+        await interaction.deferReply();
+        const guild = interaction.guild ?? await interaction.client.guilds.fetch(interaction.guildId);
+        await guild.channels.fetchActiveThreads();
+        await db.IgnoreChannel.bulkCreate(
+            guild.channels.cache
+                .filter(channel => channel.isText())
+                .map(channel => ({
+                    id: channel.id,
+                    GuildId: interaction.guildId
+                })),
+            { ignoreDuplicates: true }
+        );
+        logMessage(await interaction.editReply("Okay."));
     }
 };
