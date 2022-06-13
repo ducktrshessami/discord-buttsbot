4b610d93fd372778e5b3976d2eeedad674ea40bb
Josh Fish <jwee761@gmail.com>
2022-06-12 23:17:06 -0700

    unignorechannel slash callback

--- discord/commands/slash/unignorechannel.js
+++ discord/commands/slash/unignorechannel.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Permissions } = require("discord.js");
+const db = require("../../../models");
+const logMessage = require("../../utils/logMessage");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,6 +10,16 @@ module.exports = {
         .setDMPermission(false)
         .setDefaultMemberPermissions(Permissions.FLAGS.MANAGE_CHANNELS),
     callback: async function (interaction) {
-
+        let reply;
+        await interaction.deferReply();
+        const ignoreModel = await db.IgnoreChannel.findByPk(interaction.channelId);
+        if (ignoreModel) {
+            await ignoreModel.destroy();
+            reply = `Okay ${interaction.client.responseEmojis.smile}`;
+        }
+        else {
+            reply = "I'm not ignoring this channel!";
+        }
+        logMessage(await interaction.editReply(reply));
     }
 };
