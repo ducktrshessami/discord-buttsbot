96a62fcaa0795d9d4aaff2d59e6cd307a8c80bbb
Josh Fish <jwee761@gmail.com>
2022-06-12 23:32:34 -0700

    invite slash verbose

--- discord/commands/slash/invite.js
+++ discord/commands/slash/invite.js
@@ -1,14 +1,15 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageActionRow, MessageButton } = require("discord.js");
+const logMessage = require("../../utils/logMessage");
 const { permissionValue } = require("../../../config/bot.json");
-
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("invite")
         .setDescription("I'll send a link so you can invite me somewhere else!"),
-    callback: function (interaction) {
-        return interaction.reply({
+    callback: async function (interaction) {
+        logMessage(await interaction.reply({
+            fetchReply: true,
             content: `Invite me to another server!`,
             components: [new MessageActionRow({
                 components: [new MessageButton({
@@ -17,6 +18,6 @@ module.exports = {
                     url: `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=${permissionValue}&scope=bot%20applications.commands`
                 })]
             })]
-        });
+        }));
     }
 };
