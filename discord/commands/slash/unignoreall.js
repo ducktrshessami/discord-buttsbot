6b4bfc09cf3381d5328cb8680cfe5e852b247269
Josh Fish <jwee761@gmail.com>
2022-06-12 23:04:12 -0700

    unignoreall slash callback

--- discord/commands/slash/unignoreall.js
+++ discord/commands/slash/unignoreall.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Permissions } = require("discord.js");
+const db = require("../../../models");
+const logMessage = require("../../utils/logMessage");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,6 +10,10 @@ module.exports = {
         .setDMPermission(false)
         .setDefaultMemberPermissions(Permissions.FLAGS.MANAGE_GUILD),
     callback: async function (interaction) {
-
+        await interaction.deferReply();
+        await db.IgnoreChannel.destroy({
+            where: { GuildId: interaction.guildId }
+        });
+        logMessage(await interaction.editReply(`Okay ${interaction.client.responseEmojis.smile}`));
     }
 };
