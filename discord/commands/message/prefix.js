895b3530a65fefd387f08ab0f1c13400b3547ea9
Josh Fish <jwee761@gmail.com>
2022-06-15 00:54:35 -0700

    prefix message data

---
+++ discord/commands/message/prefix.js
@@ -0,0 +1,11 @@
+module.exports = {
+    data: {
+        name: "prefix",
+        description: "View or change the command prefix!",
+        args: "[prefix]",
+        requireGuild: true
+    },
+    callback: async function (message, args, guildModel) {
+
+    }
+};
