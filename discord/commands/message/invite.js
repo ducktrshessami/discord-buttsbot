741b2fd1aa1c568c3aa8843c4d5c02d12517a420
Josh Fish <jwee761@gmail.com>
2022-06-15 13:16:40 -0700

    invite message data

---
+++ discord/commands/message/invite.js
@@ -0,0 +1,9 @@
+module.exports = {
+    data: {
+        name: "invite",
+        description: "I'll send a link so you can invite me somewhere else!"
+    },
+    callback: async function (message) {
+
+    }
+};
