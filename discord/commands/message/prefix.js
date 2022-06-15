7fec19cba84ed6cd0a173a724b7f0351f0f43c78
Josh Fish <jwee761@gmail.com>
2022-06-15 00:55:03 -0700

    prefix message callback

--- discord/commands/message/prefix.js
+++ discord/commands/message/prefix.js
@@ -1,3 +1,5 @@
+const { Permissions } = require("discord.js");
+
 module.exports = {
     data: {
         name: "prefix",
@@ -6,6 +8,25 @@ module.exports = {
         requireGuild: true
     },
     callback: async function (message, args, guildModel) {
-
+        let reply;
+        const newValue = args[1];
+        if (newValue) {
+            if (message.channel.permissionsFor(message.member).has(Permissions.FLAGS.MANAGE_GUILD)) {
+                const { prefix } = await guildModel.update({ prefix: newValue });
+                reply = `Custom prefix changed to \`${prefix}\`!`;
+            }
+            else {
+                reply = `You are missing the following permissions:\n\`Manage Server\``;
+            }
+        }
+        else {
+            if (guildModel.prefix) {
+                reply = `The custom prefix is currently \`${guildModel.prefix}\`!`;
+            }
+            else {
+                reply = "Custom prefix not set.";
+            }
+        }
+        logMessage(await message.reply(reply));
     }
 };
