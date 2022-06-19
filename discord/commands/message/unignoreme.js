eef4a48edb30d093f6e5a2d1a00b798f2b64fb94
Josh Fish <jwee761@gmail.com>
2022-06-19 14:39:07 -0700

    unignoreme message callback

--- discord/commands/message/unignoreme.js
+++ discord/commands/message/unignoreme.js
@@ -1,9 +1,21 @@
+const db = require("../../../models");
+const logMessage = require("../../utils/logMessage");
+
 module.exports = {
     data: {
         name: "unignoreme",
         description: "Undo ignoreme!"
     },
     callback: async function (message) {
-
+        let reply;
+        const ignoredUser = await db.IgnoreUser.findByPk(message.author.id);
+        if (ignoredUser) {
+            await ignoredUser.destroy();
+            reply = `Okay ${message.client.responseEmojis.smile}`;
+        }
+        else {
+            reply = "I'm not ignoring you!";
+        }
+        logMessage(await message.reply(reply));
     }
 };
