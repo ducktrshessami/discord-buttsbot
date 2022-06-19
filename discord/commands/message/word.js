f50fe620a6958d20de51eeff4427afc1aa22527a
Josh Fish <jwee761@gmail.com>
2022-06-19 15:22:27 -0700

    word message callback

--- discord/commands/message/word.js
+++ discord/commands/message/word.js
@@ -1,4 +1,6 @@
 const { Permissions } = require("discord.js");
+const db = require("../../../models");
+const logMessage = require("../../utils/logMessage");
 const defaultButt = require("../../../config/default.json");
 
 module.exports = {
@@ -10,6 +12,21 @@ module.exports = {
         requirePermissions: Permissions.FLAGS.MANAGE_GUILD
     },
     callback: async function (message, args, guildModel) {
-
+        let reply;
+        const newValue = args[1]?.toLowerCase();
+        const guildModel = await db.Guild.findByPk(message.guildId);
+        if (newValue) {
+            if (/\s/g.test(newValue)) {
+                reply = "No spaces, please!";
+            }
+            else {
+                const { word } = await guildModel.update({ word: newValue });
+                reply = `Buttification word changed to \`${word}\`!`;
+            }
+        }
+        else {
+            reply = `I buttify messages with the word \`${guildModel.word}\`!`;
+        }
+        logMessage(await message.reply(reply));
     }
 };
