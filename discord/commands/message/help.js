77e8a1caf7e8b72ecdc1a10a7efcbf450e1493b7
Josh Fish <jwee761@gmail.com>
2022-06-19 21:31:09 -0700

    help message command

---
+++ discord/commands/message/help.js
@@ -0,0 +1,10 @@
+module.exports = {
+    data: {
+        name: "help",
+        description: "Show a command list or more info about a specific command.",
+        args: "[command name]"
+    },
+    callback: async function (message, args) {
+
+    }
+};
