77b1e5c5634badd006d3c86220d3517b0a5a3296
Josh Fish <jwee761@gmail.com>
2022-06-19 22:10:57 -0700

    help message callback

--- discord/commands/message/help.js
+++ discord/commands/message/help.js
@@ -1,3 +1,8 @@
+const getCommandListPage = require("../../utils/getCommandListPage");
+const logMessage = require("../../utils/logMessage");
+const permissionText = require("../../utils/permissionText");
+const messageCommands = require("./index");
+
 module.exports = {
     data: {
         name: "help",
@@ -5,6 +10,18 @@ module.exports = {
         args: "[command name]"
     },
     callback: async function (message, args) {
-
+        let reply;
+        const commandName = args[1]?.toLowerCase();
+        if (commandName) {
+            const command = messageCommands.get(commandName);
+            reply = `\`@${message.client.user.username} ${command.data.name} ${command.data.args || ""}`.trim() +
+                `\`\n**${command.data.name}:** ${command.data.description}\n${command.data.subtitle || ""}`.trim() +
+                (command.data.requireGuild || command.data.requirePermissions ? "\nYou can only use this command in a server." : "") +
+                (command.data.requirePermissions ? ` Also you need the ${permissionText(command.data.requirePermissions)}!` : "")
+        }
+        else {
+            reply = getCommandListPage(false);
+        }
+        logMessage(await message.reply(reply));
     }
 };
