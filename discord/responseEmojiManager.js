c1a89b6ca19e6740f66426cda587e724c4c10f0c
Josh Fish <jwee761@gmail.com>
2022-07-19 12:38:27 -0700

    response emoji manager handle factory

--- discord/responseEmojiManager.js
+++ discord/responseEmojiManager.js
@@ -6,37 +6,20 @@ function checkPermissions(channel, interaction = false) {
             .has(Permissions.FLAGS.USE_EXTERNAL_EMOJIS);
 }
 
+function responseFactory(envEmote, defaultEmote) {
+    return function (repliable) {
+        if (process.env[envEmote] && checkPermissions(repliable)) {
+            return process.env[envEmote];
+        }
+        else {
+            return defaultEmote;
+        }
+    };
+}
+
 module.exports = {
-    smile: function (channel, interaction = false) {
-        if (process.env.RES_SMILE && checkPermissions(channel, interaction)) {
-            return process.env.RES_SMILE;
-        }
-        else {
-            return ":D";
-        }
-    },
-    frown: function (channel, interaction = false) {
-        if (process.env.RES_FROWN && checkPermissions(channel, interaction)) {
-            return process.env.RES_FROWN;
-        }
-        else {
-            return ":(";
-        }
-    },
-    wink: function (channel, interaction = false) {
-        if (process.env.RES_WINK && checkPermissions(channel, interaction)) {
-            return process.env.RES_WINK;
-        }
-        else {
-            return ";)";
-        }
-    },
-    weird: function (channel, interaction = false) {
-        if (process.env.RES_WEIRD && checkPermissions(channel, interaction)) {
-            return process.env.RES_WEIRD;
-        }
-        else {
-            return "O_o";
-        }
-    }
+    smile: responseFactory("RES_SMILE", ":D"),
+    frown: responseFactory("RES_FROWN", ":("),
+    wink: responseFactory("RES_WINK", ";)"),
+    weird: responseFactory("RES_WEIRD", "O_o")
 };
