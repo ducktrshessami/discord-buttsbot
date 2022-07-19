8b701ac37b116bf0c9bc95f4354b464f1aebdbd2
Josh Fish <jwee761@gmail.com>
2022-07-18 17:10:34 -0700

    build script styling

--- build.js
+++ build.js
@@ -11,20 +11,21 @@ const { Client } = require("discord.js");
 const botConfig = require("./config/bot.json");
 
 const index = resolve(__dirname, "public", "index.html");
-const client = new Client({ intents: [] });
+const client = new Client({ intents: 0 });
 
 if (fs.existsSync(index)) {
-    client.on("ready", () => {
-        console.log(`[discord] Logged in as ${client.user.tag}`);
-        let built = fs.readFileSync(index)
-            .toString("utf8")
-            .replace("{{ client_id }}", client.user.id)
-            .replace("{{ permission_value }}", botConfig.permissionValue);
-        fs.writeFileSync(index, built);
-        console.log("[build] Successfully built /public/index.html");
-        client.destroy();
-        console.log("[discord] Logged out");
-    })
+    client
+        .on("ready", () => {
+            console.log(`[discord] Logged in as ${client.user.tag}`);
+            let built = fs.readFileSync(index)
+                .toString("utf8")
+                .replace("{{ client_id }}", client.user.id)
+                .replace("{{ permission_value }}", botConfig.permissionValue);
+            fs.writeFileSync(index, built);
+            console.log("[build] Successfully built /public/index.html");
+            client.destroy();
+            console.log("[discord] Logged out");
+        })
         .login()
         .catch(err => {
             console.error(err);
