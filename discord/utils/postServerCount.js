22f6403480af22aed63ae93bf874d4d15f22cb2b
Josh Fish <jwee761@gmail.com>
2022-07-28 18:20:07 -0700

    post with undici

--- discord/utils/postServerCount.js
+++ discord/utils/postServerCount.js
@@ -1,18 +1,21 @@
-const phin = require("phin");
+const { request } = require("undici");
+const { STATUS_CODES } = require("http");
 
 // TODO: Total guild polling with sharding when guild count surpasses 1500
 async function postServerCount(client) {
     if (process.env.TOP_TOKEN) {
         console.log("[discord] Posting server count to Top.gg");
         try {
-            const res = await phin({
-                url: `https://top.gg/api/bots/${client.user.id}/stats`,
+            const res = await request(`https://top.gg/api/bots/${client.user.id}/stats`, {
                 method: "POST",
-                headers: { Authorization: process.env.TOP_TOKEN },
-                data: { server_count: client.guilds.cache.size }
+                headers: {
+                    Authorization: process.env.TOP_TOKEN,
+                    "content-type": "application/json"
+                },
+                body: `{"server_count":${client.guilds.cache.size}}`
             });
             if (res.statusCode < 200 || res.statusCode >= 300) {
-                throw new Error(`${res.statusCode} ${res.statusMessage}`);
+                throw new Error(`${res.statusCode} ${STATUS_CODES[res.statusCode]}`);
             }
         }
         catch (error) {
