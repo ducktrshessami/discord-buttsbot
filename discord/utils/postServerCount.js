b33615263cae31a8e2ba517d1c0d99174d653448
Josh Fish <jwee761@gmail.com>
2022-08-01 14:00:50 -0700

    refactor postServerCount with comments for scaling

--- discord/utils/postServerCount.js
+++ discord/utils/postServerCount.js
@@ -1,25 +1,23 @@
 const { request } = require("undici");
 const { STATUS_CODES } = require("http");
 
-// TODO: Total guild polling with sharding when guild count surpasses 1500
 async function postServerCount(client) {
     if (process.env.TOP_TOKEN) {
+        // console.log("[discord] Polling server count");
+        // const results = await client.shard.fetchClientValues("guilds.cache.size");
+        // const guildCount = results.reduce((total, count) => total + count, 0);
+        const guildCount = client.guilds.cache.size;
         console.log("[discord] Posting server count to Top.gg");
-        try {
-            const res = await request(`https://top.gg/api/bots/${client.user.id}/stats`, {
-                method: "POST",
-                headers: {
-                    Authorization: process.env.TOP_TOKEN,
-                    "content-type": "application/json"
-                },
-                body: `{"server_count":${client.guilds.cache.size}}`
-            });
-            if (res.statusCode < 200 || res.statusCode >= 300) {
-                throw new Error(`${res.statusCode} ${STATUS_CODES[res.statusCode]}`);
-            }
-        }
-        catch (error) {
-            console.error(error);
+        const res = await request(`https://top.gg/api/bots/${client.user.id}/stats`, {
+            method: "POST",
+            headers: {
+                Authorization: process.env.TOP_TOKEN,
+                "content-type": "application/json"
+            },
+            body: `{"server_count":${guildCount}}`
+        });
+        if (res.statusCode < 200 || res.statusCode >= 300) {
+            throw new Error(`${res.statusCode} ${STATUS_CODES[res.statusCode]}`);
         }
     }
 }
