1cc10df890a1b5848b6b0743fec986db3253d5a4
Josh Fish <jwee761@gmail.com>
2022-08-16 17:52:57 -0700

    removed express server

--- index.js
+++ index.js
@@ -5,21 +5,8 @@ catch {
     console.warn("Not using dotenv. Make sure environment variables are set");
 }
 
-const express = require("express");
-const cycle = require("express-cycle");
-const { resolve } = require("path");
 const db = require("./models");
 const { responseCooldown } = require("./config/bot.json");
-
-const app = express();
-const PORT = process.env.PORT || 8080;
-const cycler = cycle({
-    origin: process.env.PUBLIC_ORIGIN || `http://localhost:${PORT}`,
-    verbose: true
-});
-
-app.use(cycler);
-app.use(express.static(resolve(__dirname, "public")));
 
 db.sequelize.sync({ force: process.env.DB_FORCE && process.env.DB_FORCE.trim().toLowerCase() !== "false" })
     .then(() => {
@@ -32,11 +19,7 @@ db.sequelize.sync({ force: process.env.DB_FORCE && process.env.DB_FORCE.trim().t
             }
         });
     })
-    .then(() => app.listen(PORT, function () {
-        console.log(`[express] Listening on PORT ${PORT}`);
-        cycler.startLoop();
-        require("./discord");
-    }))
+    .then(() => require("./discord"))
     .catch(err => {
         console.error(err);
         process.exit();
