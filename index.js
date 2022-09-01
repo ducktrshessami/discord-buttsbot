2559a6b8634361717732e585efa35618577b872c
Josh Fish <jwee761@gmail.com>
2022-08-31 20:48:10 -0700

    fix wrong cooldown column

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ async function main() {
     console.log("[db] Syncing tables with models");
     await db.sync(process.env.DB_FORCE && process.env.DB_FORCE.trim().toLowerCase() !== "false");
     console.log("[db] Pruning old cooldown data");
-    await db.execute(`DELETE FROM "${db.models.ResponseCooldown.tableName}" WHERE "${db.models.ResponseCooldown.tableName}"."createdAt" < ${Date.now() - (responseCooldown * 2)}`);
+    await db.execute(`DELETE FROM "${db.models.ResponseCooldown.tableName}" WHERE "${db.models.ResponseCooldown.tableName}"."updatedAt" < ${Date.now() - (responseCooldown * 2)}`);
     require("./discord");
 }
 
