9d9fe0449354ce6dd163c330b08db0c7fc17664b
Josh Fish <jwee761@gmail.com>
2022-08-31 17:13:54 -0700

    responsecooldown model rewrite

--- models/responsecooldown.js
+++ models/responsecooldown.js
@@ -1,31 +1,27 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class ResponseCooldown extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  ResponseCooldown.init({
-    channelId: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-      allowNull: false
-    },
-    smile: DataTypes.DATE,
-    frown: DataTypes.DATE,
-    wink: DataTypes.DATE,
-    weird: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'ResponseCooldown',
-  });
-  return ResponseCooldown;
+"use strict";
+const { Model, DataTypes } = require("nessie");
+module.exports = (nessie) => {
+    class ResponseCooldown extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    ResponseCooldown.init({
+        channelId: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false
+        },
+        smile: DataTypes.NUMBER,
+        frown: DataTypes.NUMBER,
+        wink: DataTypes.NUMBER,
+        weird: DataTypes.NUMBER,
+        updatedAt: DataTypes.NUMBER
+    }, { nessie });
+    return ResponseCooldown;
 };
