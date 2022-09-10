9851d660cc3ee035c1698d5c137063e6bf9dbf99
Josh Fish <jwee761@gmail.com>
2022-09-10 09:34:25 -0700

    reintroduce node env to db

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const { Nessie } = require("nessie");
 const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
 
 const db = new Nessie({
     verbose: true,
@@ -18,7 +19,7 @@ fs
     .filter(file => {
         return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
-    .forEach(file => require(path.join(__dirname, file))(db));
+    .forEach(file => require(path.join(__dirname, file))(db, env));
 
 Object.keys(db.models).forEach(modelName => {
     if (db.models[modelName].associate) {
