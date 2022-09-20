9bc5e1842ac6feea6804f37d9c23b0b1953ac401
Josh Fish <jwee761@gmail.com>
2022-09-20 10:37:58 -0700

    revert model loader

--- models/index.js
+++ models/index.js
@@ -1,29 +1,37 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
-const path = require("path");
-const { Nessie } = require("nessie");
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/db.json')[env];
+const db = {};
 
-const db = new Nessie({
-    verbose: true,
-    libDir: process.env.DB_LIBDIR,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    connectionString: process.env.DB_CONNECTSTRING
-});
+let sequelize;
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
     .readdirSync(__dirname)
     .filter(file => {
         return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
-    .forEach(file => require(path.join(__dirname, file))(db));
+    .forEach(file => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+    });
 
-Object.keys(db.models).forEach(modelName => {
-    if (db.models[modelName].associate) {
-        db.models[modelName].associate(db.models);
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
     }
 });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
