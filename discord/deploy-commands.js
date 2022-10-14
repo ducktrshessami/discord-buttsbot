afa1899525cf09f112f16096a3511a6502df62dd
Josh Fish <jwee761@gmail.com>
2022-10-14 16:24:13 -0700

    always deploy commands globally

--- discord/deploy-commands.js
+++ discord/deploy-commands.js
@@ -8,16 +8,10 @@ catch {
 const { REST, Routes } = require("discord.js");
 const commands = require("./commands");
 
-const env = process.env.DISCORD_ENV || "development";
 const commandData = commands.map(command => command.data.toJSON());
 const rest = new REST({ version: "10" })
     .setToken(process.env.DISCORD_TOKEN);
 
-rest.put(
-    env === "production" ?
-        Routes.applicationCommands(process.env.DISCORD_CLIENTID) :
-        Routes.applicationGuildCommands(process.env.DISCORD_CLIENTID, process.env.DISCORD_TESTGUILD),
-    { body: commandData }
-)
+rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENTID), { body: commandData })
     .then(() => console.log("Successfully deployed commands."))
     .catch(console.error);
