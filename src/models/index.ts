c800d184806aacccab172d6e9667e9dcdc487ae2
ducktrshessami <jwee761@gmail.com>
2023-04-23 22:12:08 -0700

    init models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,10 @@
 import { Sequelize } from "sequelize";
 import config from "../config.js";
 import { NODE_ENV } from "../constants.js";
+import Guild from "./Guild.js";
+import IgnoreUser from "./IgnoreUser.js";
+import IgnoreChannel from "./IgnoreChannel.js";
+import ResponseCooldown from "./ResponseCooldown.js";
 
 const dbConfig = config.db[NODE_ENV];
 
@@ -10,3 +14,18 @@ if (dbConfig.use_env_variable) {
 } else {
     sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
 }
+
+Guild.initialize(sequelize);
+IgnoreUser.initialize(sequelize);
+IgnoreChannel.initialize(sequelize);
+ResponseCooldown.initialize(sequelize);
+
+Guild.hasMany(IgnoreChannel, { onDelete: "CASCADE" });
+IgnoreChannel.belongsTo(Guild);
+
+export {
+    Guild,
+    IgnoreUser,
+    IgnoreChannel,
+    ResponseCooldown
+};
