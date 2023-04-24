e545b5d9b63033784572534d1ecbed20c4610a3e
ducktrshessami <jwee761@gmail.com>
2023-04-23 22:05:11 -0700

    declare ResponseCooldown model

---
+++ src/models/ResponseCooldown.ts
@@ -0,0 +1,32 @@
+import {
+    CreationOptional,
+    DataTypes,
+    InferAttributes,
+    InferCreationAttributes,
+    Model,
+    Sequelize
+} from "sequelize";
+
+export default class ResponseCooldown extends Model<InferAttributes<ResponseCooldown>, InferCreationAttributes<ResponseCooldown>> {
+    declare channelId: string;
+    declare smile: CreationOptional<Date>;
+    declare frown: CreationOptional<Date>;
+    declare wink: CreationOptional<Date>;
+    declare weird: CreationOptional<Date>;
+
+    static initialize(sequelize: Sequelize): void {
+        this.init({
+            channelId: {
+                type: DataTypes.STRING,
+                primaryKey: true
+            },
+            smile: DataTypes.DATE,
+            frown: DataTypes.DATE,
+            wink: DataTypes.DATE,
+            weird: DataTypes.DATE
+        }, {
+            sequelize,
+            modelName: "ResponseCooldown"
+        });
+    }
+}
