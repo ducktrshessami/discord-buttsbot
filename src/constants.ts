45efb56bd14cb74499a2e24e342cf728c63ffdfe
ducktrshessami <jwee761@gmail.com>
2023-04-25 18:48:08 -0700

    typed presence config

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,4 @@ await dotenv();
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const DB_FORCE = !!process.env.DB_FORCE && process.env.DB_FORCE.trim().toLowerCase() !== "false";
 export const TOP_ENDPOINT = `https://top.gg/api/bots/${process.env.DISCORD_CLIENT_ID}/stats`;
+export const PRESENCE_INTERVAL = parseInt(process.env.PRESENCE_INTERVAL!) || 1800000;
