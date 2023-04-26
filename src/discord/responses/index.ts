10db6ffe789f924c4015ae448c57afa26b3755b5
ducktrshessami <jwee761@gmail.com>
2023-04-25 18:41:00 -0700

    responses array instead of collection

--- src/discord/responses/index.ts
+++ src/discord/responses/index.ts
@@ -1,23 +1,19 @@
-import { Collection } from "discord.js";
 import { readdirSync } from "fs";
 import { basename } from "path";
 import { fileURLToPath } from "url";
 
 const indexBasename = basename(import.meta.url);
-const responses = new Collection<string, EmojiResponse>(
-    await Promise.all(
-        readdirSync(fileURLToPath(new URL(".", import.meta.url)))
-            .filter(file =>
-                (file.indexOf(".") !== 0) &&
-                (file !== indexBasename) &&
-                (file.slice(-3) === ".js")
-            )
-            .map(async (file): Promise<[string, EmojiResponse]> => {
-                const url = new URL(file, import.meta.url);
-                const res: EmojiResponse = await import(url.toString());
-                return [res.emoji, res];
-            })
-    )
+const responses = await Promise.all(
+    readdirSync(fileURLToPath(new URL(".", import.meta.url)))
+        .filter(file =>
+            (file.indexOf(".") !== 0) &&
+            (file !== indexBasename) &&
+            (file.slice(-3) === ".js")
+        )
+        .map((file): Promise<EmojiResponse> => {
+            const url = new URL(file, import.meta.url);
+            return <Promise<EmojiResponse>>import(url.toString());
+        })
 );
 export default responses;
 
