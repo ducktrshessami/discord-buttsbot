12c0570d33038a8673e0420e0de7dcec5400a2a5
ducktrshessami <jwee761@gmail.com>
2023-04-26 15:24:10 -0700

    use envInt util

--- src/constants.ts
+++ src/constants.ts
@@ -5,11 +5,11 @@ await dotenv();
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const DB_FORCE = !!process.env.DB_FORCE && process.env.DB_FORCE.trim().toLowerCase() !== "false";
 export const TOP_ENDPOINT = `https://top.gg/api/bots/${process.env.DISCORD_CLIENT_ID}/stats`;
-export const PRESENCE_INTERVAL = parseInt(process.env.PRESENCE_INTERVAL!) || 1800000;
-export const DISCORD_SWEEPER_INTERVAL = process.env.DISCORD_SWEEPER_INTERVAL ? parseInt(process.env.DISCORD_SWEEPER_INTERVAL) : 3600;
-export const DISCORD_THREAD_LIFETIME = process.env.DISCORD_THREAD_LIFETIME ? parseInt(process.env.DISCORD_THREAD_LIFETIME) : 3600;
-export const DISCORD_MESSAGE_LIFETIME = process.env.DISCORD_MESSAGE_LIFETIME ? parseInt(process.env.DISCORD_MESSAGE_LIFETIME) : 3600;
-export const DISCORD_LIMITED_CACHE_MAX = process.env.DISCORD_LIMITED_CACHE_MAX ? parseInt(process.env.DISCORD_LIMITED_CACHE_MAX) : 100;
+export const PRESENCE_INTERVAL = envInt("PRESENCE_INTERVAL", 1800000);
+export const DISCORD_SWEEPER_INTERVAL = envInt("DISCORD_SWEEPER_INTERVAL", 3600);
+export const DISCORD_THREAD_LIFETIME = envInt("DISCORD_THREAD_LIFETIME", 3600);
+export const DISCORD_MESSAGE_LIFETIME = envInt("DISCORD_MESSAGE_LIFETIME", 3600);
+export const DISCORD_LIMITED_CACHE_MAX = envInt("DISCORD_LIMITED_CACHE_MAX", 100);
 export const DISCORD_RESPONSE_COOLDOWN = envInt("DISCORD_RESPONSE_COOLDOWN", 30000);
 
 function envInt(env: string, defaultValue: number): number {
