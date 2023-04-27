45d59966c13c0aaafd9e9a86c95585d6f6cb1d54
ducktrshessami <jwee761@gmail.com>
2023-04-27 13:50:38 -0700

    rate command callback

--- src/discord/commands/rate.ts
+++ src/discord/commands/rate.ts
@@ -1,5 +1,10 @@
-import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import {
+    ChatInputCommandInteraction,
+    PermissionFlagsBits,
+    SlashCommandBuilder
+} from "discord.js";
 import config from "../../config.js";
+import { getGuild, updateGuild } from "../guild.js";
 
 export const data = new SlashCommandBuilder()
     .setName("rate")
@@ -15,3 +20,16 @@ export const data = new SlashCommandBuilder()
             .setDescription(`A new rate to buttify syllables! The lower this is, the more I'll buttify! The default is ${config.default.rate}.`)
             .setMinValue(1);
     });
+
+export async function callback(interaction: ChatInputCommandInteraction<"cached">): Promise<void> {
+    await interaction.deferReply();
+    const newValue = interaction.options.getInteger("value");
+    if (newValue) {
+        await updateGuild(interaction.guildId, { rate: newValue });
+        await interaction.editReply(`Buttify rate changed to one in every \`${newValue}\` syllables per buttified message!`);
+    }
+    else {
+        const guildModel = await getGuild(interaction.guildId);
+        await interaction.editReply(`I buttify roughly one in every \`${guildModel?.rate ?? config.default.rate}\` syllables per buttified message!`);
+    }
+}
