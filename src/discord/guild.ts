b1f2880fb3c0ab848b56302da7812e25863d91aa
ducktrshessami <jwee761@gmail.com>
2023-04-27 00:02:52 -0700

    guild config update util

--- src/discord/guild.ts
+++ src/discord/guild.ts
@@ -8,6 +8,19 @@ export async function initializeGuild(guildId: string, transaction?: Transaction
     });
 }
 
+export async function updateGuild(guildId: string, values: Omit<Parameters<typeof Guild.create<Guild>>[0], "id">): Promise<void> {
+    await Guild.bulkCreate([{
+        ...values,
+        id: guildId
+    }], {
+        updateOnDuplicate: [
+            "frequency",
+            "rate",
+            "word"
+        ]
+    });
+}
+
 export async function deleteGuild(guildId: string): Promise<void> {
     await Guild.destroy({
         where: { id: guildId }
