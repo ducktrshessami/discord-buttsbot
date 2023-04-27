212559fce87ab2f51aeaeebd6d726cee54756d54
ducktrshessami <jwee761@gmail.com>
2023-04-27 15:00:33 -0700

    remove old source

--- discord/responses/smile.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    emoji: "smile",
-    keywords: [
-        "yes",
-        "yeah",
-        "yea"
-    ]
-};
--- discord/responses/weird.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    emoji: "weird",
-    keywords: ["why"]
-};
