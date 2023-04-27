e66d6b0c82fbc9b45003df8f16a0e424cdb727d9
ducktrshessami <jwee761@gmail.com>
2023-04-27 14:01:38 -0700

    word command callback

--- src/discord/commands/word.ts
+++ src/discord/commands/word.ts
@@ -1,5 +1,12 @@
-import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import {
+    ChatInputCommandInteraction,
+    PermissionFlagsBits,
+    SlashCommandBuilder
+} from "discord.js";
 import config from "../../config.js";
+import { getGuild, updateGuild } from "../guild.js";
+
+const WhitespacePattern = /\s/;
 
 export const data = new SlashCommandBuilder()
     .setName("word")
@@ -14,3 +21,23 @@ export const data = new SlashCommandBuilder()
             .setName("value")
             .setDescription(`A new word to buttify with! No spaces, please! The default is ${config.default.word}!`);
     });
+
+export async function callback(interaction: ChatInputCommandInteraction<"cached">) {
+    await interaction.deferReply();
+    const newValue = interaction.options
+        .getString("value")
+        ?.toLowerCase();
+    if (newValue) {
+        if (WhitespacePattern.test(newValue)) {
+            await interaction.editReply("No spaces, please!");
+        }
+        else {
+            await updateGuild(interaction.guildId, { word: newValue });
+            await interaction.editReply(`Buttification word changed to \`${newValue}\`!`);
+        }
+    }
+    else {
+        const guildModel = await getGuild(interaction.guildId);
+        await interaction.editReply(`I buttify messages with the word \`${guildModel?.word ?? config.default.word}\`!`);
+    }
+}
