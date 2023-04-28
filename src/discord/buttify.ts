200b9416c9b5edccf30e10e4331cdfde30dd4e31
ducktrshessami <jwee761@gmail.com>
2023-04-27 22:08:25 -0700

    rewrite buttification with multiple attempts

--- src/discord/buttify.ts
+++ src/discord/buttify.ts
@@ -3,44 +3,130 @@ import { syllablize } from "fast-syllablize";
 import { IgnoreUser } from "../models/index.js";
 import config from "../config.js";
 
+const ExpectedProbability = 0.95;
 const NonWordPattern = /(https?:\/\/(?:www\.)?[-A-Z0-9@:%._\+~#=]{1,256}(?:\.[A-Z0-9()]{1,6})?\b(?:[-A-Z0-9()@:%_\+.~#?&\/=]*)|<?(?:a?:?\w{2,32}:|#|@[!&]?)\d{17,19}>?|[^A-Z]+)/ig;
 const WordPattern = /^[A-Z]+$/i;
 const CapsPattern = /^[A-Z]$/;
 const AllCapsPattern = /^[A-Z]{2,}$/;
 const PluralPattern = /[SZ]$/i;
 
+class ContentItem<Word extends boolean = boolean> {
+    private readonly word: Word;
+    private readonly allCaps: Word extends true ? boolean : false;
+    private readonly pluralChar: string | null;
+    private readonly _syllables: Word extends true ? Array<string> : null;
+    private _current: Word extends true ? string : null;
+    private _buttified: number;
+
+    constructor(private readonly content: ButtifiedContent, public readonly chars: string) {
+        this.word = <typeof this.word>WordPattern.test(chars);
+        this.allCaps = <typeof this.allCaps>(this.word ? AllCapsPattern.test(chars) : false);
+        this.pluralChar = this.word &&
+            !this.content.pluralWord &&
+            PluralPattern.test(chars[chars.length - 1]) ?
+            chars[chars.length - 1] :
+            null;
+        this._syllables = <typeof this._syllables>(this.word ? syllablize(chars) : null);
+        this._current = <typeof this._current>(this.word ? chars : null);
+        this._buttified = 0;
+        this.buttify();
+    }
+
+    get syllables(): number {
+        return this._syllables?.length ?? 0;
+    }
+
+    get buttified(): number {
+        return this._buttified;
+    }
+
+    isWord(): this is ContentItem<true> {
+        return this.word;
+    }
+
+    buttify(): string {
+        if (!this.isWord()) {
+            return this.chars;
+        }
+        this._buttified = 0;
+        this._current = <typeof this._current>this._syllables.reduce(
+            (
+                buttified,
+                syllable,
+                i,
+                syllables
+            ) => {
+                if (chance(this.content.rate)) {
+                    let word = this.content.word;
+                    this._buttified++;
+                    if (this.pluralChar && i === syllables.length - 1) {
+                        word += this.pluralChar;
+                    }
+                    if (this.allCaps) {
+                        buttified += word.toUpperCase();
+                    }
+                    else for (let j = 0; j < word.length; j++) {
+                        buttified += CapsPattern.test(syllable[j]) ? word[j].toUpperCase() : word[j];
+                    }
+                }
+                else {
+                    buttified += syllable;
+                }
+                return buttified;
+            },
+            ""
+        );
+        return this._current;
+    }
+
+    toString(): string {
+        return this._current ?? this.chars;
+    }
+}
+
+class ButtifiedContent {
+    readonly pluralWord: boolean;
+    private readonly items: Array<ContentItem>;
+
+    constructor(
+        readonly original: string,
+        readonly word: string,
+        readonly rate: number
+    ) {
+        this.pluralWord = PluralPattern.test(word[word.length - 1]);
+        this.items = original
+            .split(NonWordPattern)
+            .reduce((items, item) => {
+                if (item) {
+                    items.push(new ContentItem(this, item));
+                }
+                return items;
+            }, new Array<ContentItem>());
+    }
+
+    get syllables(): number {
+        return this.items.reduce((syllables, item) => syllables + item.syllables, 0);
+    }
+
+    get buttified(): number {
+        return this.items.reduce((buttified, item) => buttified + item.buttified, 0);
+    }
+
+    buttify(): string {
+        return this.items.reduce((buttified, item) => buttified + item.buttify(), "");
+    }
+
+    toString(): string {
+        return this.items.join("");
+    }
+}
+
 function chance(n: number): boolean {
     return Math.random() < (1 / n);
 }
 
-function buttifyWord(
-    original: string,
-    word: string,
-    rate: number
-): string {
-    const allCaps = AllCapsPattern.test(original);
-    if (PluralPattern.test(original[original.length - 1])) {
-        word += original[original.length - 1].toLowerCase();
-    }
-    return syllablize(original)
-        .map(syllable => {
-            if (chance(rate)) {
-                if (allCaps) {
-                    return word.toUpperCase();
-                }
-                else {
-                    let buttified = "";
-                    for (let i = 0; i < word.length; i++) {
-                        buttified += CapsPattern.test(syllable[i]) ? word[i].toUpperCase() : word[i];
-                    }
-                    return buttified;
-                }
-            }
-            else {
-                return syllable;
-            }
-        })
-        .join("");
+function attempts(rate: number, syllables: number): number {
+    return Math.ceil(Math.log(1 - ExpectedProbability) / (syllables * Math.log((rate - 1) / rate)));
 }
 
 export function buttify(
@@ -48,18 +134,19 @@ export function buttify(
     word: string = config.default.word,
     rate: number = config.default.rate
 ): string {
-    return content
-        .split(NonWordPattern)
-        .map(chars =>
-            WordPattern.test(chars) ?
-                buttifyWord(
-                    chars,
-                    word,
-                    rate
-                ) :
-                chars ?? ""
-        )
-        .join("");
+    const buttifiedContent = new ButtifiedContent(
+        content,
+        word,
+        rate
+    );
+    if (buttifiedContent.syllables < 2) {
+        return content;
+    }
+    const maxAttempts = attempts(rate, buttifiedContent.syllables);
+    for (let i = 1; i < maxAttempts && !buttifiedContent.buttified; i++) {
+        buttifiedContent.buttify();
+    }
+    return buttifiedContent.toString();
 }
 
 export async function buttifiable(message: Message<true>, frequency: number = config.default.frequency): Promise<boolean> {
