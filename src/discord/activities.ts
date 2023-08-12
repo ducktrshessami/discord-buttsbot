7c7ec196cb3e681a96c108499decba7ecb65aec2
ducktrshessami <jwee761@gmail.com>
2023-08-11 19:31:00 -0700

    feat: new activities

--- src/discord/activities.ts
+++ src/discord/activities.ts
@@ -12,6 +12,21 @@ const activities: Array<ActivitiesOptions> = [
     {
         type: ActivityType.Playing,
         name: "hard to get"
+    },
+    {
+        // @ts-expect-error
+        type: ActivityType.Custom,
+        name: "On the streets fighting for the Bowel Movement"
+    },
+    {
+        // @ts-expect-error
+        type: ActivityType.Custom,
+        name: "Tossing a salad"
+    },
+    {
+        // @ts-expect-error
+        type: ActivityType.Custom,
+        name: "Eating a peach"
     }
 ];
 export default activities;
