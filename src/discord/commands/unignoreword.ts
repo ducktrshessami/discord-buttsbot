727c21f78d65bee6e120e3cee7f4a27e9238caab
ducktrshessami <cal.tf2.3@gmail.com>
2024-11-14 22:30:36 -0800

    refactor: split ignoreword/unignoreword

--- src/discord/commands/unignoreword.ts
+++ src/discord/commands/unignoreword.ts
@@ -3,14 +3,15 @@ import { resolvePermissionString } from "../util.js";
 
 export const data: RESTPostAPIApplicationCommandsJSONBody = {
     type: ApplicationCommandType.ChatInput,
-    name: "ignoreword",
-    description: "I will never buttify this word.",
+    name: "unignoreword",
+    description: "Undo ignoreword!",
     contexts: [InteractionContextType.Guild],
     default_member_permissions: resolvePermissionString(PermissionFlagsBits.ManageGuild),
     options: [{
         type: ApplicationCommandOptionType.String,
         name: "word",
-        description: "The word for me to ignore.",
-        required: true
+        description: "The word for me to stop ignoring!",
+        required: true,
+        autocomplete: true
     }]
 };
