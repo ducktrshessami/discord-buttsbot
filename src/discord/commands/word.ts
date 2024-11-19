9e7a55485c5495592f4225c2f5aabcaace320b1e
ducktrshessami <cal.tf2.3@gmail.com>
2024-11-19 10:58:46 -0800

    refactor: word data

--- src/discord/commands/word.ts
+++ src/discord/commands/word.ts
@@ -1,27 +1,30 @@
 import {
+    ApplicationCommandOptionType,
+    ApplicationCommandType,
     ChatInputCommandInteraction,
     InteractionContextType,
     PermissionFlagsBits,
-    SlashCommandBuilder
+    RESTPostAPIChatInputApplicationCommandsJSONBody
 } from "discord.js";
 import config from "../../config.js";
 import { getGuild, updateGuild } from "../guild.js";
+import { resolvePermissionString } from "../util.js";
 
 const WhitespacePattern = /\s/;
 
-export const data = new SlashCommandBuilder()
-    .setName("word")
-    .setDescription("Use this command to show or change what word I buttify messages with!")
-    .setContexts(InteractionContextType.Guild)
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
-    .addStringOption(option => {
-        if (config.limit.wordLength > 0) {
-            option.setMaxLength(config.limit.wordLength);
-        }
-        return option
-            .setName("value")
-            .setDescription(`A new word to buttify with! No spaces, please! The default is ${config.default.word}!`);
-    });
+export const data: RESTPostAPIChatInputApplicationCommandsJSONBody = {
+    type: ApplicationCommandType.ChatInput,
+    name: "word",
+    description: "Use this command to show or change what word I buttify messages with!",
+    contexts: [InteractionContextType.Guild],
+    default_member_permissions: resolvePermissionString(PermissionFlagsBits.ManageGuild),
+    options: [{
+        type: ApplicationCommandOptionType.String,
+        name: "value",
+        description: `A new word to buttify with! No spaces, please! The default is ${config.default.word}!`,
+        max_length: config.limit.wordLength > 0 ? config.limit.wordLength : undefined
+    }]
+};
 
 export async function callback(interaction: ChatInputCommandInteraction<"cached">): Promise<void> {
     await interaction.deferReply();
