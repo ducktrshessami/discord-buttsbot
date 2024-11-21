4593f4fb8f712b5031a51e35a326676548a08ee6
ducktrshessami <cal.tf2.3@gmail.com>
2024-11-20 17:36:59 -0800

    feat: ignoreword callbacK

--- src/discord/commands/ignoreword.ts
+++ src/discord/commands/ignoreword.ts
@@ -1,11 +1,13 @@
 import {
     ApplicationCommandOptionType,
     ApplicationCommandType,
+    ChatInputCommandInteraction,
     InteractionContextType,
     PermissionFlagsBits,
     RESTPostAPIApplicationCommandsJSONBody
 } from "discord.js";
-import { resolvePermissionString } from "../util.js";
+import { ignoreWord } from "../ignore.js";
+import { resolvePermissionString, WhitespacePattern } from "../util.js";
 
 export const data: RESTPostAPIApplicationCommandsJSONBody = {
     type: ApplicationCommandType.ChatInput,
@@ -20,3 +22,17 @@ export const data: RESTPostAPIApplicationCommandsJSONBody = {
         required: true
     }]
 };
+
+export async function callback(interaction: ChatInputCommandInteraction<"cached">): Promise<void> {
+    await interaction.deferReply();
+    const word = interaction.options
+        .getString("word", true)
+        .toLowerCase();
+    if (WhitespacePattern.test(word)) {
+        await interaction.editReply("No spaces, please!");
+    }
+    else {
+        const ignored = await ignoreWord(word, interaction.guildId);
+        await interaction.editReply(ignored ? "Okay." : "I'm already ignoring that word.");
+    }
+}
