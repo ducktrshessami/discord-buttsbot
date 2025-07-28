b515a74d32f2fbe6c3d84afe460af76ebea32aaf
ducktrshessami <cal.tf2.3@gmail.com>
2025-07-27 23:21:44 -0700

    chore: remove dotenv loading

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,3 @@
-import dotenv from "./dotenv.js";
-
-await dotenv();
-
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const DB_FORCE = !!process.env.DB_FORCE && process.env.DB_FORCE.trim().toLowerCase() !== "false";
 export const TOP_ENDPOINT = `https://top.gg/api/bots/${process.env.DISCORD_CLIENT_ID}/stats`;
